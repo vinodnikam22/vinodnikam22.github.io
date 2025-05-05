55d7153d06d68e65374092177be97e6a15c81ec8
Vinod Nikam <38093106+vinodnikam22@users.noreply.github.com>
2025-05-05 08:32:58 +0530

    Add files via upload

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -12,13 +12,13 @@ $phone = isset($_POST['phone']) ? strip_tags(htmlspecialchars($_POST['phone'])) 
 $message = isset($_POST['message']) ? strip_tags(htmlspecialchars($_POST['message'])) : '';
 
 // Create the email and send the message
-$to = 'yourname@yourdomain.com';
+$to = 'info@nextremesystems.com';
 $email_subject = "Website Contact Form: $name";
 $email_body = "You have received a new message from your website contact form.\n\n".
               "Here are the details:\n\nName: $name\n\nEmail: $email_address\n\n".
               "Phone: $phone\n\nMessage:\n$message";
               
-$headers = "From: noreply@yourdomain.com\r\n";
+$headers = "From: noreply@nextremesystems.com\r\n";
 $headers .= "Reply-To: $email_address\r\n";
 $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
